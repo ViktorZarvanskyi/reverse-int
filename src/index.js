b83c3e396c9ec52b2fc365fd3a3d6f55d66a5f6e
Viktor Zarvanskyi <viktor.zarvanskyi@gmail.com>
2022-06-26 14:03:00 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        return Number(n.toString().split('').reverse().join(''));
+    } else {
+        let newN = n * -1;
+        return Number(newN.toString().split('').reverse().join(''));
+    }
 }
